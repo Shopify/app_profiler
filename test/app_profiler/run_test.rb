8894ca7078432dedc18fddfc50ee059cadf9232a
Gannon McGibbon <gannon.mcgibbon@shopify.com>
2021-02-16 16:29:30 -0500

    Update rubocop

--- test/app_profiler/run_test.rb
+++ test/app_profiler/run_test.rb
@@ -6,7 +6,7 @@ module AppProfiler
   class RunTest < TestCase
     test ".run delegates to Profiler.run" do
       profile = AppProfiler.run(stackprof_profile(mode: :cpu, interval: 2000)) do
-        sleep 0.1
+        sleep(0.1)
       end
 
       assert_instance_of(AppProfiler::Profile, profile)
