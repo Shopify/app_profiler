7101fa465754e0df09b3998ff9bb6847bd61217d
Dale Hamel <dale.hamel@shopify.com>
2023-10-19 22:30:48 +0000

    Add remaining test cases to vernier backend

--- test/app_profiler/backend/vernier_backend_test.rb
+++ test/app_profiler/backend/vernier_backend_test.rb
@@ -66,131 +66,135 @@ module AppProfiler
       assert_equal("bar", profile.context)
     end
 
+    test ".run cpu profile" do
+      skip "on-CPU mode not yet supported by vernier" # https://github.com/jhawthorn/vernier/issues/29
+      profile = Profiler.run(vernier_profile(mode: :cpu, interval: 2000)) do
+        sleep(0.1)
+      end
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:cpu, profile[:mode])
+      assert_equal(2000, profile[:interval])
+    end
+
+    test ".run wall profile" do
+      profile = Profiler.run(vernier_profile(mode: :wall, interval: 2000)) do
+        sleep(0.1)
+      end
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:wall, profile[:mode])
+      # assert_equal(2000, profile[:interval]) # TODO as above
+    end
+
+    test ".run object profile" do
+      skip "object allocation mode not yet supported by vernier"
+      profile = Profiler.run(stackprof_profile(mode: :object, interval: 2)) do
+        sleep(0.1)
+      end
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:object, profile[:mode])
+      assert_equal(2, profile[:interval])
+    end
+
+    test ".start uses wall profile by default" do
+      Profiler.start
+      Profiler.stop
+
+      profile = Profiler.results
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:wall, profile[:mode])
+      # assert_equal(1000, profile[:interval])
+    end
+
+    test ".start assigns metadata to profiles" do
+      Profiler.start(vernier_profile(metadata: { id: "wowza", context: "bar" }))
+      Profiler.stop
+
+      profile = Profiler.results
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal("wowza", profile.id)
+      assert_equal("bar", profile.context)
+    end
+
+    test ".start cpu profile" do
+      skip "on-CPU mode not yet supported by vernier" # https://github.com/jhawthorn/vernier/issues/29
+      Profiler.start(stackprof_profile(mode: :cpu, interval: 2000))
+      Profiler.stop
+
+      profile = Profiler.results
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:cpu, profile[:mode])
+      assert_equal(2000, profile[:interval])
+    end
+
+    test ".start wall profile" do
+      Profiler.start(vernier_profile(mode: :wall, interval: 2000))
+      Profiler.stop
+
+      profile = Profiler.results
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:wall, profile[:mode])
+      # assert_equal(2000, profile[:interval])
+    end
+
+    test ".start object profile" do
+      skip "object allocation mode not yet supported by vernier"
+      Profiler.start(vernier_profile(mode: :object, interval: 2))
+      Profiler.stop
+
+      profile = Profiler.results
+
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_equal(:object, profile[:mode])
+      assert_equal(2, profile[:interval])
+    end
+
+    test ".stop" do
+      AppProfiler.start
+      Vernier::Collector.any_instance.expects(:stop)
+      AppProfiler.stop
+    end
+
+    test ".results prints error when failed" do
+      AppProfiler.profiler_backend.expects(:backend_results).returns({})
+      AppProfiler.logger.expects(:info).with { |value| value =~ /failed to obtain the profile/ }
+
+      assert_nil(Profiler.results)
+    end
+
+    test ".results returns nil when profiling is still active" do
+      Profiler.run do
+        assert_nil(Profiler.results)
+      end
+    end
+
+    test ".start, .stop, and .results interact well" do
+      AppProfiler.logger.expects(:info).never
+
+      assert_equal(true, Profiler.start)
+      assert_equal(false, Profiler.start)
+      assert_equal(true, Profiler.send(:running?))
+      assert_nil(Profiler.results)
+      assert_equal(true, Profiler.stop)
+      assert_equal(false, Profiler.stop)
+      assert_equal(false, Profiler.send(:running?))
+
+      profile = Profiler.results
+      assert_instance_of(AppProfiler::VernierProfile, profile)
+      assert_predicate(profile, :valid?)
+
+      assert_nil(Profiler.results)
+    end
+
     def teardown
       AppProfiler.profiler_backend = @orig_backend
       @orig_backend = nil
     end
   end
 end
-#
-#    test ".run cpu profile" do
-#      profile = Profiler.run(stackprof_profile(mode: :cpu, interval: 2000)) do
-#        sleep(0.1)
-#      end
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:cpu, profile[:mode])
-#      assert_equal(2000, profile[:interval])
-#    end
-#
-#    test ".run wall profile" do
-#      profile = Profiler.run(stackprof_profile(mode: :wall, interval: 2000)) do
-#        sleep(0.1)
-#      end
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:wall, profile[:mode])
-#      assert_equal(2000, profile[:interval])
-#    end
-#
-#    test ".run object profile" do
-#      profile = Profiler.run(stackprof_profile(mode: :object, interval: 2)) do
-#        sleep(0.1)
-#      end
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:object, profile[:mode])
-#      assert_equal(2, profile[:interval])
-#    end
-#
-#    test ".start uses cpu profile by default" do
-#      Profiler.start(stackprof_profile)
-#      Profiler.stop
-#
-#      profile = Profiler.results
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:cpu, profile[:mode])
-#      assert_equal(1000, profile[:interval])
-#    end
-#
-#    test ".start assigns metadata to profiles" do
-#      Profiler.start(stackprof_profile(metadata: { id: "wowza", context: "bar" }))
-#      Profiler.stop
-#
-#      profile = Profiler.results
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal("wowza", profile.id)
-#      assert_equal("bar", profile.context)
-#    end
-#
-#    test ".start cpu profile" do
-#      Profiler.start(stackprof_profile(mode: :cpu, interval: 2000))
-#      Profiler.stop
-#
-#      profile = Profiler.results
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:cpu, profile[:mode])
-#      assert_equal(2000, profile[:interval])
-#    end
-#
-#    test ".start wall profile" do
-#      Profiler.start(stackprof_profile(mode: :wall, interval: 2000))
-#      Profiler.stop
-#
-#      profile = Profiler.results
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:wall, profile[:mode])
-#      assert_equal(2000, profile[:interval])
-#    end
-#
-#    test ".start object profile" do
-#      Profiler.start(stackprof_profile(mode: :object, interval: 2))
-#      Profiler.stop
-#
-#      profile = Profiler.results
-#
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_equal(:object, profile[:mode])
-#      assert_equal(2, profile[:interval])
-#    end
-#
-#    test ".stop" do
-#      StackProf.expects(:stop)
-#      AppProfiler.stop
-#    end
-#
-#    test ".results prints error when failed" do
-#      AppProfiler.profiler_backend.expects(:backend_results).returns({})
-#      AppProfiler.logger.expects(:info).with { |value| value =~ /failed to obtain the profile/ }
-#
-#      assert_nil(Profiler.results)
-#    end
-#
-#    test ".results returns nil when profiling is still active" do
-#      Profiler.run(stackprof_profile) do
-#        assert_nil(Profiler.results)
-#      end
-#    end
-#
-#    test ".start, .stop, and .results interact well" do
-#      AppProfiler.logger.expects(:info).never
-#
-#      assert_equal(true, Profiler.start(stackprof_profile))
-#      assert_equal(false, Profiler.start(stackprof_profile))
-#      assert_equal(true, Profiler.send(:running?))
-#      assert_nil(Profiler.results)
-#      assert_equal(true, Profiler.stop)
-#      assert_equal(false, Profiler.stop)
-#      assert_equal(false, Profiler.send(:running?))
-#
-#      profile = Profiler.results
-#      assert_instance_of(AppProfiler::Profile, profile)
-#      assert_predicate(profile, :valid?)
-#
-#      assert_nil(Profiler.results)
-#    end
-#  end
