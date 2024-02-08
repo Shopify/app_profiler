c1f685d5045037f905ce44210716482327de1b11
Dale Hamel <dale.hamel@shopify.com>
2024-02-08 15:30:41 -0500

    Move VernierProfile to using hash instead of Results object

--- lib/app_profiler/profile/vernier.rb
+++ lib/app_profiler/profile/vernier.rb
@@ -4,33 +4,8 @@ module AppProfiler
   class VernierProfile < Profile
     FILE_EXTENSION = ".gecko.json"
 
-    delegate :[], to: :@meta
-
-    attr_reader :data
-
-    def initialize(data, id: nil, context: nil)
-      @meta = data.meta
-      super
-    end
-
-    # https://github.com/jhawthorn/vernier/blob/main/lib/vernier/result.rb#L27-L29
-    def file
-      @file ||= path.tap do |p|
-        p.dirname.mkpath
-        @data.write(out: p)
-      end
-    end
-
-    def valid?
-      !@data.nil?
-    end
-
-    def to_h
-      @data.to_h
-    end
-
     def mode
-      @meta[:mode]
+      @data[:meta][:mode]
     end
 
     def format
