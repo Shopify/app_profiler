0c49fd33f4ba183ae938aa1a7c5e0053947a33ba
Dale Hamel <dale.hamel@shopify.com>
2024-04-16 13:13:21 -0400

    Add explicit require statement for active_support deprecation usage

--- lib/app_profiler/profile.rb
+++ lib/app_profiler/profile.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/deprecation/constant_accessor"
+
 module AppProfiler
   autoload :StackprofProfile, "app_profiler/profile/stackprof"
   autoload :VernierProfile, "app_profiler/profile/vernier"
